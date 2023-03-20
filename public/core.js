4a236c2005470a1823121c83789953ed53eee453
Your Name <you@example.com>
2023-03-20 19:17:29 +0530

    indexdone

--- public/core.js
+++ public/core.js
@@ -12,8 +12,15 @@ const firebaseConfig = {
   };
     firebase.initializeApp(firebaseConfig);
     //globals
-mainmark = 0
-mainouts = 0
+mainmark = 0;
+mainouts = 0;
+ball=0
+ocount=0
+bcount=0
+wideno=0
+bowlerballs = 0
+bowlerrate = 0
+noticemsg = "none";
     // Get a reference to the database
 const database = firebase.database();
 
@@ -23,10 +30,65 @@ const bat1 = document.getElementById('bat1');
 const bat2 = document.getElementById('bat2');
 const bowname = document.getElementById('bowname');
 
+database.ref('scores/last/marks').on('value', snapshot => {
+array1 = snapshot.val()
+console.log(snapshot.val())
+
+for (let i =1; i < 9; i++) {
+
+  if(array1["ball"+i]=="wd"){
+    $$('dot'+i).src= "images/wd.png"
+    wideno= wideno+1
+  }
+  if(array1["ball"+i]=="nb"){
+    $$('dot'+i).src= "images/nb.png"
+    wideno= wideno+1
+  }
+  if(parseInt(array1["ball"+i])==0){
+    $$('dot'+i).src= "images/0.png"
+  }
+  if(wideno==1){
+    $("#dot7").fadeIn();
+  } else{
+    if(wideno==2){
+    $("#dot7").fadeIn();
+    $("#dot8").fadeIn();} else{
+      $("#dot7").fadeOut();
+      $("#dot8").fadeOut();
+    }
+  }
+  if(array1["ball"+i]=='non'){
+    $$('dot'+i).src= "images/empty.png"
+  } else{
+    if(parseInt(array1["ball"+i])){
+      $$('dot'+i).src= "images/"+parseInt(array1["ball"+i])+".png"
+    }
+  }
+  if(i==8){
+    wideno=0
+  }
+}
+
+
+});
+
 database.ref('scores/main/marks').on('value', snapshot => {
     const score = snapshot.val();
     mainmark = score;
     scorelabel.textContent = mainmark+"-"+mainouts;
+    
+    var newb= parseInt(bcount)*2
+    if(newb==10){
+      newb=9
+    }
+    console.log(newb)
+    totover= ocount+"."+newb
+    brun = parseInt(mainmark)/parseInt(totover).toFixed(2)
+    if (brun=='Infinity'){
+      brun=0
+    }
+    $$("runrate").innerText = "RUN RATE "+ brun.toFixed(1) ;
+    $$('smallal').style.backgroundColor = "#1c1052";
   });
 
   database.ref('scores/main/out').on('value', snapshot => {
@@ -34,14 +96,78 @@ database.ref('scores/main/marks').on('value', snapshot => {
     mainouts = score;
     scorelabel.textContent = mainmark+"-"+mainouts;
 
+
+
+    
     database.ref('player/bat1/name').on('value', snapshot => {
         bat1.textContent = snapshot.val();
+        batter1 = snapshot.val();
+        database.ref('bats/'+batter1+'/score').on('value', snapshot => {
+          if(snapshot.val()!=null){
+            $$('bat1mark').innerText =     snapshot.val();
+          } else{
+            $$('bat1mark').innerText =  0;
+
+          }
+            })
+            database.ref('bats/'+batter1+"/balls").on('value', snapshot => {
+              if(snapshot.val()!=null){
+                $$('bat1balls').innerText = snapshot.val()
+              } else{
+                $$('bat1balls').innerText =   00;
+    
+              }
+          
+             });
+             
       });
       database.ref('player/bat2/name').on('value', snapshot => {
         bat2.textContent = snapshot.val();
+        batter2 = snapshot.val();
+        database.ref('bats/'+batter2+'/score').on('value', snapshot => {
+          if(snapshot.val()!=null){
+            $$('bat2mark').innerText =     snapshot.val();
+          } else{
+            $$('bat2mark').innerText =   00;
+
+          }
+            })
+            database.ref('bats/'+batter2+"/balls").on('value', snapshot => {
+              if(snapshot.val()!=null){
+                $$('bat2balls').innerText =     snapshot.val();
+              } else{
+                $$('bat2balls').innerText =   00;
+    
+              }
+             });
       });
+
       database.ref('player/bow/name').on('value', snapshot => {
         bowname.textContent = snapshot.val();
+        bowlernm =  snapshot.val();
+        database.ref('balls/'+bowlernm+'/score').on('value', snapshot => {
+          if(snapshot.val()!=null){
+            bowlerrate = parseInt(snapshot.val())
+    $$('bowlerrate').innerText = bowlerrate + "-"+bowlerballs;
+
+          } else{
+                    bowlerrate = 0
+                    $$('bowlerrate').innerText = bowlerrate + "-"+bowlerballs;
+
+
+          }
+            })
+            database.ref('balls/'+bowlernm+"/balls").on('value', snapshot => {
+              if(snapshot.val()!=null){
+                bowlerballs = parseInt( snapshot.val())
+    $$('bowlerrate').innerText = bowlerrate + "-"+bowlerballs;
+
+              } else{
+                bowlerballs = 0
+    $$('bowlerrate').innerText = bowlerrate + "-"+bowlerballs;
+              }
+          
+             });
       });
       database.ref('stats/arrow').on('value', snapshot => {
         var arrn = snapshot.val()
@@ -66,3 +192,87 @@ if (a==1){
 }
   }
  
+  database.ref('notice/head/msg').on('value', snapshot => {
+    noticemsg = snapshot.val();
+    $$('noticetext').innerText  =snapshot.val();
+  });
+  database.ref('notice/visible/bool').on('value', snapshot => {
+if(parseInt(snapshot.val())==0){
+  $("#noticeboard").fadeIn();
+  console.log(snapshot.val())
+} else{
+  $("#noticeboard").fadeOut();
+  console.log(snapshot.val())
+
+}
+  });
+  database.ref('nowbat/schl').on('value', snapshot => {
+    notbatno = parseInt(snapshot.val())
+    if(notbatno==1){
+      $$('nowplayingtext').innerText = "RC"
+      $$('image01').src = "images/ukraine-flag-png-xl.png";
+      $$('image02').src = "images/images.png";
+    } else{
+      $$('nowplayingtext').innerText = "PD"
+      $$('image02').src = "images/ukraine-flag-png-xl.png";
+      $$('image01').src = "images/images.png";
+
+    }
+    
+    ;
+ 
+  });
+
+  database.ref('overs/count').on('value', snapshot => {
+    ocount = snapshot.val();
+   $$('ballno').innerText = ocount+"."+bcount
+  });
+
+  database.ref('overs/ball').on('value', snapshot => {
+    if( $$("dot7").style.display=="none"&& $$("dot8").style.display=="none"){
+      thislimit = 6
+    } else{
+      if( $$("dot7").style.display!="none"&& $$("dot8").style.display=="none"){
+        thislimit = 7
+      }  else{
+        if( $$("dot7").style.display!="none"&& $$("dot8").style.display!="none"){
+          thislimit = 8
+        } 
+      }
+    }
+    bcount = snapshot.val();
+    if (parseInt(bcount)>=thislimit){
+      bcount = 0;
+      ocount = ocount+1;
+      $("#dot7").fadeOut();
+      $("#dot8").fadeOut();
+    }
+   $$('ballno').innerText = parseInt(ocount)+"."+parseInt(bcount)
+  });
+
+  database.ref('notes/pop').on('value', snapshot => {
+    if(snapshot.val()!="-"){
+      $$('smallal').style.backgroundColor = "#f60c0c"
+      $$('runrate').innerText = snapshot.val();
+    }
+
+    
+   
+  })
+
+
+
+
+  database.ref('notes/vis').on('value', snapshot => {
+ 
+    if(snapshot.val()=="hide"){
+      $$('smallal').style.backgroundColor = "#1c1052"
+      $("#runrate").fadeOut();
+    } else{
+      $("#smallal").fadeIn();
+      $("#runrate").fadeIn();
+    }
+  
+ 
+})
+
--- public/dash.js
+++ public/dash.js
@@ -1,7 +1,14 @@
 var $$ = function( id ) { return document.getElementById( id ); };
- toggleb = 1
- mainmark = 0
+//variables
+toggleb = 1
+mainmark = 0
 mainouts = 0
+noteon = 0
+nowbat = 1
+over=0
+ball=0
+limit=6
+alertl=0
 const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
@@ -27,13 +34,13 @@ const firebaseConfig = {
     const database = firebase.database();
 
 function players(){
-    database.ref('player/bat1/').set({
+    database.ref('player/bat1/').update({
 name:$$('player1choose').value
     })
-    database.ref('player/bat2/').set({
+    database.ref('player/bat2/').update({
         name:$$('player2choose').value
             })
-            database.ref('player/bow/').set({
+            database.ref('player/bow/').update({
                 name:$$('bowchoose').value
                     }).then(
                         Toast.fire({
@@ -43,6 +50,21 @@ name:$$('player1choose').value
                     )
 }
 
+database.ref('stats/arrow').on('value', snapshot => {
+    toggleb = parseInt(snapshot.val())
+  });
+database.ref('nowbat/schl').on('value', snapshot => {
+    nowbat = parseInt(snapshot.val())
+  });
+
+database.ref('overs/count').on('value', snapshot => {
+    over = parseInt(snapshot.val())
+  });
+
+  database.ref('overs/ball').on('value', snapshot => {
+    ball = parseInt(snapshot.val())
+  });
+  
 function marksmain(){
     if ($$("mainscoretext").value!=''&&$$("mainouttext").value!=''){
         database.ref('scores/main/marks').transaction(score =>$$("mainscoretext").value);
@@ -64,22 +86,376 @@ function togglebat(){
 if  (toggleb ==1){
     toggleb = 2;
     database.ref('stats/arrow').transaction(score =>2);
+    database.ref('player/bat1/name').once('value', snapshot => {
+        const rat1 = snapshot.val();
+        Toast.fire({
+            icon: 'success',
+            title: rat1 + ' is now batting.'
+          }) 
+      })
+  
 } else{
     toggleb = 1;
     database.ref('stats/arrow').transaction(score =>1); 
+    database.ref('player/bat2/name').once('value', snapshot => {
+        const rat2 = snapshot.val();
+        Toast.fire({
+            icon: 'success',
+            title: rat2 + ' is now batting.'
+          }) 
+      })
 }
 }
 database.ref('scores/main/marks').on('value', snapshot => {
     const score = snapshot.val();
     mainmark = score;
     scorelabel.textContent = mainmark+"-"+mainouts;
+    $$('mainscoretext').value = mainmark
+    
   });
 
   database.ref('scores/main/out').on('value', snapshot => {
     const score = snapshot.val();
     mainouts = score;
+    $$('mainouttext').value = mainouts
     scorelabel.textContent = mainmark+"-"+mainouts;  });
 
-    function quickm(b){
-        database.ref('scores/main/marks').transaction(score =>parseInt(score)+parseInt(b));
+    function clrdots() {
+        for (let i = 0; i < 9; i++) {
+            console.log(i+"ay")
+            database.ref('scores/last/marks').update(JSON.parse('{"ball'+i+'":"non"}')).then(
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Reset dots successfully'
+                  }) 
+            )
+            
+        }   
+        ball = 0
     }
+
+    function quickm(b){
+
+        
+        database.ref('balls/'+bowler+'/balls').once('value', snapshot => {
+            if(snapshot.val()==null){
+                bowlerballs = 0
+            }else{
+                bowlerballs  = parseInt(snapshot.val())
+            }
+            database.ref('balls/'+bowler).update({
+                balls:bowlerballs+1
+            }); 
+
+            database.ref('balls/'+bowler+'/score').once('value', snapshot => {
+                if(snapshot.val()==null){
+                    bowlermark = 0
+                }else{
+                    bowlermark  = parseInt(snapshot.val())
+                }
+                database.ref('balls/'+bowler).update({
+                    score:bowlermark+parseInt(b)
+                }); 
+            })
+        })
+
+        database.ref('scores/main/marks').transaction(score =>parseInt(score)+parseInt(b));
+        if(parseInt(b)==4){
+            database.ref('notes/').update({
+                pop:"4 FOUR"
+            })
+            database.ref('notes/').update({
+                pop:"-"
+            })
+        } else{
+            if(parseInt(b)==6){
+                database.ref('notes/').update({
+                    pop:"6 SIX"
+                })
+                database.ref('notes/').update({
+                    pop:"-"
+                })
+            }
+        }
+    
+      if(ball<(limit)){
+
+        ball=ball+1;
+        database.ref('overs/').update({
+            ball:ball
+        })
+        $$('ballno').innerText = "Ball No: "+ ball
+        console.log(ball+"ball")
+
+        database.ref('scores/last/marks').update(JSON.parse('{"ball'+ball+'":"'+b+'"}'))
+    } else{
+        for (let i = 0; i < 9; i++) {
+            console.log(i+"ay")
+            database.ref('scores/last/marks').update(JSON.parse('{"ball'+i+'":"non"}')).then(
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Next Over'
+                  }) 
+            )
+            
+        }
+        //next over
+        over= over+1
+        limit=6
+        database.ref('overs/').update({
+            count:over
+        })
+
+
+        $$('ballno').innerText = "Ball No: 1"
+
+        ball=1
+        database.ref('overs/').update({
+            ball:ball
+        })
+        database.ref('scores/last/marks').update(JSON.parse('{"ball1":"'+b+'"}'))
+    }
+
+    if(parseInt(toggleb)==2){
+
+        database.ref('bats/'+batter1+'/balls').once('value', snapshot => {
+            if(snapshot.val()==null){
+                bat1balls = 0
+            }else{
+                bat1balls  = parseInt(snapshot.val())
+            }
+            database.ref('bats/'+batter1).update({
+                balls:bat1balls+1
+            }); 
+
+            database.ref('bats/'+batter1+'/score').once('value', snapshot => {
+                if(snapshot.val()==null){
+                    bat1mark = 0
+                }else{
+                    bat1mark  = parseInt(snapshot.val())
+                }
+                database.ref('bats/'+batter1).update({
+                    score:bat1mark+parseInt(b)
+                }); 
+            })
+        })
+
+  
+       
+
+    } else{
+        database.ref('bats/'+batter2+'/score').once('value', snapshot => {
+            if(snapshot.val()==null){
+                bat2mark = 0
+            }else{
+                bat2mark  = parseInt(snapshot.val())
+            }
+            database.ref('bats/'+batter2).update({
+                score:bat2mark+parseInt(b)
+            }); 
+            database.ref('bats/'+batter2+'/balls').once('value', snapshot => {
+                if(snapshot.val()==null){
+                    bat2balls = 0
+                }else{
+                    bat2balls  = parseInt(snapshot.val())
+                }
+                database.ref('bats/'+batter2).update({
+                    balls:bat2balls+1
+                }); 
+            })
+        
+        })
+    }
+   
+    
+
+}
+
+    function notice(){
+       var note = document.getElementById("tempnote").value;
+       database.ref('notice/head').update({
+        msg:note
+    }).then(
+        Toast.fire({
+            icon: 'success',
+            title: 'Message Saved successfully'
+          })
+    )
+}
+
+function shownote(){
+if (noteon==0){
+    noteon=1;
+    $$('notebtn').innerText = "SHOW";
+    database.ref('notice/visible').update({
+bool:1
+    })
+} else{
+    noteon=0;
+    $$('notebtn').innerText = "HIDE";
+    database.ref('notice/visible').update({
+bool:0
+    }) 
+}
+}
+
+function flagchange(){
+if (nowbat==2){
+    nowbat=1
+    database.ref('nowbat/').update({
+        schl:1
+    }).then(
+        Toast.fire({
+            icon: 'success',
+            title: 'Rahula College is Batting Now'
+          }))
+} else{
+    nowbat=2
+    database.ref('nowbat/').update({
+        schl:2
+    }).then(
+        Toast.fire({
+            icon: 'success',
+            title: 'Pannipitiya Dharmapala is Batting Now'
+          }))
+}
+}
+
+function updateball(){
+    if($$('ballc').value!=''){
+        database.ref('overs/').update({
+            ball:$$('ballc').value
+        }).then(
+            Toast.fire({
+                icon: 'success',
+                title: 'Updated Balss'
+              }) 
+        )
+        ball = parseInt($$('ballc').value)
+    } else{
+        Toast.fire({
+            icon: 'error',
+            title: 'Bad Input'
+          })
+    }
+   
+}
+
+
+function updateover(){
+    if($$('overc').value!=''){
+        database.ref('overs/').update({
+            count:$$('overc').value
+        }).then(
+            Toast.fire({
+                icon: 'success',
+                title: 'Updated Over'
+              }) 
+        )
+        over = parseInt($$('overc').value)
+    } else{
+        Toast.fire({
+            icon: 'error',
+            title: 'Bad Input'
+          })
+    }
+   
+}
+
+function wide (){
+    function repwide(){
+        if(ball<(limit)){
+    
+            ball=ball+1;
+            database.ref('overs/').update({
+                ball:ball
+            })
+            $$('ballno').innerText = "Ball No: "+ ball
+            console.log(ball+"ball")
+    
+            database.ref('scores/last/marks').update(JSON.parse('{"ball'+ball+'":"wd"}'))
+        database.ref('scores/main/marks').transaction(score =>parseInt(score)+1);
+    
+        }
+    }
+if(limit==6){
+    limit=7
+    repwide()
+} else{
+    if(limit==7){
+        limit=8
+        repwide()
+    }  else{
+        alert("Max 2 dots")
+    }
+   
+
+    
+}
+}
+
+function noball (){
+    function repwide(){
+        if(ball<(limit)){
+    
+            ball=ball+1;
+            database.ref('overs/').update({
+                ball:ball
+            })
+            $$('ballno').innerText = "Ball No: "+ ball
+            console.log(ball+"ball")
+    
+            database.ref('scores/last/marks').update(JSON.parse('{"ball'+ball+'":"nb"}'))
+        database.ref('scores/main/marks').transaction(score =>parseInt(score)+1);
+    
+        }
+    }
+if(limit==6){
+    limit=7
+    repwide()
+} else{
+    if(limit==7){
+        limit=8
+        repwide()
+    }  else{
+        alert("Max 2 dots")
+    }
+   
+
+    
+}
+}
+
+function alerttog(){
+if(alertl==0){
+    alertl=1
+    database.ref('notes/').update({
+        vis:"hide"
+    })
+} else{
+    alertl=0
+    database.ref('notes/').update({
+        vis:"show"
+    })
+}
+}
+
+database.ref('player/bat1/name').on('value', snapshot => {
+     batter1 = snapshot.val();
+     $$('player1choose').value = snapshot.val()
+  })
+
+  
+database.ref('player/bat2/name').on('value', snapshot => {
+    batter2 = snapshot.val();
+    $$('player2choose').value = snapshot.val()
+
+ })
+
+ database.ref('player/bow/name').on('value', snapshot => {
+    bowler = snapshot.val();
+    $$('bowchoose').value = snapshot.val()
+
+ })
+
+
